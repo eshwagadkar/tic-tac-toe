29a65cc0f89d90a1977c68e999e1ae52de6c3fd4
eshwagadkar <eshwagadkar007@gmail.com>
2025-09-27 20:32:56 +0530

    two way binding and updating a state using form function

---
+++ src/Player.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export default function Player({ name, symbol}) {
+    
+    const [isEditing, setIsEditing] = useState(false)
+    const [playerName, setPlayerName] = useState(name)
+    
+    return (
+        <li>
+            <span className="player">
+              { isEditing ? 
+                 <input type='text' value={playerName} onChange={() => setPlayerName(event.target.value)} required /> 
+                 : <span className="player-name">{playerName}</span>}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={() => setIsEditing(editing => !editing)}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    )
+}
